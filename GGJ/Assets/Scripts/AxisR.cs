0ef6a84651d0c0282882542691120da90450922f
Takius <takunori.kasai@gmail.com>
2018-01-27 04:49:26 +0900

    First commit.

---
+++ GGJ/Assets/Scripts/AxisR.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisR : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject bullet;
+
+    bool suelta;
+    const int cantBullet = 15;
+    GameObject[] bullets;
+
+	// Use this for initialization
+	void Start () {
+        suelta = true;
+        bullets = new GameObject[cantBullet];
+        for(int i = 0; i < cantBullet; i++)
+        {
+            bullets[i] = Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+            bullets[i].SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("VerticalR") < -0.2F && suelta)
+        {
+            int i = 0;
+            while (i < cantBullet)
+            {
+                if (!bullets[i].activeSelf)
+                {
+                    bullets[i].SetActive(true);
+                    bullets[i].transform.position = gameObject.transform.position;
+                    bullets[i].transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis("HorizontalR") * 90);
+                    bullets[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-Input.GetAxis("HorizontalR"), -Input.GetAxis("VerticalR")) * 50.0f, ForceMode2D.Impulse);
+                    suelta = false;
+                    break;
+                }
+                else i++;
+            }   
+        }
+        else if (Input.GetAxis("VerticalR") > -0.2F && Input.GetAxis("VerticalR") < 0)
+            suelta = true;
+    }
+}
---
+++ GGJ/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "GameController")
+            gameObject.SetActive(false);
+    }
+
+}
