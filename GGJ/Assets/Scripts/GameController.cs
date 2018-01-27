d6798730ff8b605370eb3bac4d05bc6e68d7fb51
Takius <takunori.kasai@gmail.com>
2018-01-27 16:40:36 +0900

    Add wall for the boss and UI in GameController

--- GGJ/Assets/Scripts/GameController.cs
+++ GGJ/Assets/Scripts/GameController.cs
@@ -1,16 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    [SerializeField]
+    private Sprite loopTexture;
+    GameObject[] bgLoop;
+
+    public float velGame;
+    public int score;
+    private const int lifeMax = 3;
+    public int life;
+    public int combo;
+    [SerializeField]
+    private Sprite[] Numbers;
+
+    [SerializeField]
+    private Image[] scoreNumbers;
+    [SerializeField]
+    private Image[] lifeCount;
+    [SerializeField]
+    private Image[] comboNumbers;
+
+
+    // Use this for initialization
+    void Awake () {
+        bgLoop = new GameObject[2];
+        Vector2 initPos = new Vector2(0,0);
+        for (int i = 0; i < 2; i++)
+        {
+            bgLoop[i] = new GameObject("BGLoop");
+            bgLoop[i].transform.position = initPos;
+            bgLoop[i].AddComponent<SpriteRenderer>().sprite = loopTexture;
+            bgLoop[i].transform.localScale = new Vector2(2.5f,2.5f);
+            bgLoop[i].AddComponent<BoxCollider2D>().isTrigger = true;
+            initPos = bgLoop[i].transform.position + new Vector3(0, bgLoop[i].GetComponent<BoxCollider2D>().bounds.size.y,0);
+        }
+    }
+
+    private void Start()
+    {
+        velGame = 0.08f;
+        score = 2456;
+        life = lifeMax;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        for (int i = 0; i < 2; i++)
+        {
+            bgLoop[i].transform.position -= new Vector3(0,velGame,0);
+            if(bgLoop[i].transform.position.y + bgLoop[i].GetComponent<BoxCollider2D>().bounds.size.y/2 < transform.position.y - GetComponent<BoxCollider2D>().bounds.size.y / 2)
+            {
+                bgLoop[i].transform.position += new Vector3(0, bgLoop[i].GetComponent<BoxCollider2D>().bounds.size.y*2, 0);
+            }
+        }
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreNumbers[0].sprite = Numbers[score / 100000000];
+        scoreNumbers[1].sprite = Numbers[score % 10000000 / 1000000];
+        scoreNumbers[2].sprite = Numbers[score % 1000000 / 100000];
+        scoreNumbers[3].sprite = Numbers[score % 100000 / 10000];
+        scoreNumbers[4].sprite = Numbers[score % 10000 / 1000];
+        scoreNumbers[5].sprite = Numbers[score % 1000 / 100];
+        scoreNumbers[6].sprite = Numbers[score % 100 / 10];
+        scoreNumbers[7].sprite = Numbers[score % 10 / 1];
+
+        comboNumbers[0].sprite = Numbers[score % 1000 / 100];
+        comboNumbers[1].sprite = Numbers[score % 100 / 10];
+        comboNumbers[2].sprite = Numbers[score % 10 / 1];
+    }
+
+    public void AddLife()
+    {
+        if (life != lifeMax)
+        {
+            life++;
+            lifeCount[life - 1].gameObject.SetActive(true);
+        } 
+    }
+
+    public void SubstractLife()
+    {
+        lifeCount[life - 1].gameObject.SetActive(false);
+        life--;
+    }
 }
---
+++ GGJ/Assets/Scripts/Wall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public bool stop;
+    public Vector2 targetPos;
+    private float factor;
+    private Vector2 initPos;
+    private float distance;
+    private float speed;
+
+    void Start () {
+        factor = 1.1f;
+        speed = 2.0f;
+        stop = true;
+    }
+	
+	void Update () {
+        if (factor > 1.0f)
+        {
+            if (!stop)
+            {
+                initPos = transform.position;
+                if(transform.rotation.z == 0)
+                    targetPos.y = -targetPos.y;
+                else
+                    targetPos.x = -targetPos.x;
+                distance = Vector2.Distance(initPos, targetPos);
+                factor = 0.0f;
+            }
+        }
+        else
+        {
+            factor += (speed * Time.deltaTime) / distance;
+            transform.position = Vector2.Lerp(initPos, targetPos, factor);
+        }
+    }
+
+    public void StopMoveWall(Vector2 tpos)
+    {
+        stop = !stop;
+        targetPos = tpos;
+        factor = 1.1f;
+    }
+}
