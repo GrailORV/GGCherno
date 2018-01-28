921255a0af1f4e27f258f85b09a6c3fa0643006b
SoraE1224 <sora-e1224@nifty.com>
2018-01-28 10:28:49 +0900

    fade navigator

---
+++ GGJ/Assets/Scripts/PrologueController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrologueController : MonoBehaviour
+{
+
+    [SerializeField]
+    Fade fade;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Cancel") > 0.8f)
+        {
+            fade.Start(Fade.FADE_STATUS.FADE_OUT, 1.0f, "Game");
+        }
+    }
+
+}
